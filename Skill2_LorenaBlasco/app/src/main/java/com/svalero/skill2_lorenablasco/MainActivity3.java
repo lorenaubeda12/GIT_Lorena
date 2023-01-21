b07ef946fef59028dbfa087d23fb0427d86ff087
Lore12Ubeda <lorenaubeda12@gmail.com>
2023-01-21 18:04:13 +0100

    video 31
    Ultima parte skill2
    No funciona imagenes, preguntar a Alberto

---
+++ Skill2_LorenaBlasco/app/src/main/java/com/svalero/skill2_lorenablasco/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.svalero.skill2_lorenablasco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
