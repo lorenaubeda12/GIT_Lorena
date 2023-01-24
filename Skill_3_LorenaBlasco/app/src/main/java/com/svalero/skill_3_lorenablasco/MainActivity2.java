9afd93778d1956c498e2deecb704023d63e8c1c7
Lore12Ubeda <lorenaubeda12@gmail.com>
2023-01-24 14:42:57 +0100

    Segunda parte del Skill3
    Minuto 7.23

---
+++ Skill_3_LorenaBlasco/app/src/main/java/com/svalero/skill_3_lorenablasco/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.svalero.skill_3_lorenablasco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
