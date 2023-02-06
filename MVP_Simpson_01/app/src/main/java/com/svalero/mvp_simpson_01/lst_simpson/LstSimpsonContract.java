06678b26f1d45837ed0beb457499fa6f627a254e
Lore12Ubeda <lorenaubeda12@gmail.com>
2023-02-06 12:04:28 +0100

    simpson_Cambios

--- MVP_Simpson_01/app/src/main/java/com/svalero/mvp_simpson_01/lst_simpson/LstSimpsonContract.java
+++ MVP_Simpson_01/app/src/main/java/com/svalero/mvp_simpson_01/lst_simpson/LstSimpsonContract.java
@@ -4,22 +4,26 @@ import com.svalero.mvp_simpson_01.lst_simpson.model.pojo.simpson;
 
 import java.util.ArrayList;
 
-public interface LstSimpsonContract{
+public interface LstSimpsonContract {
 
     public interface view {
         void successLstSimpson(ArrayList<simpson> lstsimpson);
+
         void errorLstSimpson(String error);
     }
 
     public interface presenter {
+        //Caso de uso
         void LstSimpson(simpson simpson);
     }
 
     public interface model {
         interface OnLstSimpsonListener {
             void onSuccess(ArrayList<simpson> lstsimpson);
+
             void onFailure(String error);
         }
+
         void LstSimpsonWS(simpson simpson, OnLstSimpsonListener onLstSimpsonListener);
     }
 }
--- MVP_Simpson_01/app/src/main/java/com/svalero/mvp_simpson_01/lst_simpson/view/LstSimpsonActivity.java
+++ MVP_Simpson_01/app/src/main/java/com/svalero/mvp_simpson_01/lst_simpson/view/LstSimpsonActivity.java
@@ -3,11 +3,16 @@ package com.svalero.mvp_simpson_01.lst_simpson.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.svalero.mvp_simpson_01.R;
+import com.svalero.mvp_simpson_01.lst_simpson.LstSimpsonContract;
+import com.svalero.mvp_simpson_01.lst_simpson.model.pojo.simpson;
 import com.svalero.mvp_simpson_01.lst_simpson.presenter.LstSimpsonPresenter;
 
-public class LstSimpsonActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class LstSimpsonActivity extends AppCompatActivity implements LstSimpsonContract.view {
     private LstSimpsonPresenter lstSimpsonPresenter;
 
     @Override
@@ -17,7 +22,13 @@ public class LstSimpsonActivity extends AppCompatActivity {
 
         initComponent();
         initPresenter();
+        initData();
         lstSimpsonPresenter.LstSimpson(null);
+    }
+
+
+    public void initData() {
+        lstSimpsonPresenter.LstSimpson(null); //SELECT * FROM Simpson;
     }
 
     public void initComponent() {
@@ -25,5 +36,17 @@ public class LstSimpsonActivity extends AppCompatActivity {
 
     public void initPresenter() {
         lstSimpsonPresenter = new LstSimpsonPresenter();
+    }
+
+    @Override
+    public void successLstSimpson(ArrayList<simpson> lstsimpson) {
+        //Adapter notify set data changed
+        //Los datos son lo que vienen de la base de datos
+        //Los datos son lo que vienen de la base de datos
+    }
+
+    @Override
+    public void errorLstSimpson(String error) {
+        Toast.makeText(this, "Los datos no han podido ser cargados", Toast.LENGTH_SHORT).show();
     }
 }
