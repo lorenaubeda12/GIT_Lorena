f19788ba316ee2220d3c40853a1bc7fbeac2e83c
Lore12Ubeda <lorenaubeda12@gmail.com>
2023-02-21 13:03:18 +0100

    21/02/2023

--- POKEMONAPI01/app/src/main/java/com/example/pokemon_api_01/lst_pokemons/view/LstPokemonAdapter.java
+++ POKEMONAPI01/app/src/main/java/com/example/pokemon_api_01/lst_pokemons/view/LstPokemonAdapter.java
@@ -1,9 +1,12 @@
 package com.example.pokemon_api_01.lst_pokemons.view;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pokemon_api_01.R;
@@ -11,7 +14,8 @@ import com.example.pokemon_api_01.lst_pokemons.entities.Pokemon;
 
 import java.util.ArrayList;
 
-public class LstPokemonAdapter {
+public class  LstPokemonAdapter extends RecyclerView.Adapter<LstPokemonAdapter.ViewHolder> {
+
     /*VIEW HOLDER (Se encarga de atender esa fila)*/
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -25,8 +29,32 @@ public class LstPokemonAdapter {
             nombreTextView = itemView.findViewById(R.id.nombreTextView);
         }
     }
-
     public void adicionarListaPokemon(ArrayList<Pokemon> lstPokemon) {
 
     }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_pokemon, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Pokemon p = dataset.get(position);
+        holder.nombreTextView.setText(p.getName());
+
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + p.getNumber() + ".png")
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.fotoImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
